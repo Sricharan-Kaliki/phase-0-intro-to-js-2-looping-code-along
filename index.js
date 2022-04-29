edeb9dd8a794e4e2fc59c920b05abf8b8290ec55
Sricharan_Kaliki <kalikisrichsanreddy@gmail.com>
2022-04-28 21:51:54 -0400

    commit tree 9ebda5da4e80587088cfca99f9dc6e1dabbe1a4c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr,name){
+    const resultArray=[];
+    for(let i=0;i<arr.length;i++){
+       
+       resultArray.push( `Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+    }
+return resultArray;
+}
+
+function countDown(n)
+{
+    for(let i=n;i>=0;i--){
+       console.log(i);
+    }
+}
